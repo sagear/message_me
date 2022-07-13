02eded788d1fa928d34eec99827d7c7f698e90db
Sagar Bandari <sagearonn@gmail.com>
2022-07-13 17:40:04 +0530

    Added scope to message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,9 @@ class MessagesController < ApplicationController
   def create_message
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to root_path
+      ActionCable.server.broadcast "chatroom_channel", 
+                                   mod_message: message_render(message)
+      # redirect_to root_path
     end
   end
 
@@ -18,6 +20,10 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
   end
 
 end
